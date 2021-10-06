ca5430e948ffd89ad2d9630b7c24ceeb98e9efa6
Serena Bertozzi <serenabertozzi@protonmail.com>
2021-10-06 17:05:31 +0100

    Set up testing infrastructure with Capybara and RSpec

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
-require 'sinatra/base'
-require 'sinatra/reloader'
+require "sinatra/base"
+require "sinatra/reloader"
 
 class Battle < Sinatra::Base
-  configure :development do 
+  configure :development do
     register Sinatra::Reloader
   end
-  
-  get '/' do 
-    'Hello Battle!'
+
+  get "/" do
+    "Testing infrastructure working!"
   end
 
   run! if app_file == $0
