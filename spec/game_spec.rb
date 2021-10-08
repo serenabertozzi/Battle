55b0c16689c152d844d07bcec248e4b25077c4f4
Serena Bertozzi <serenabertozzi@protonmail.com>
2021-10-08 15:44:21 +0100

    refactor of controller
    Co-Authored-by: Anthony <dajoxx@gmail.com>
    Co-Authored-by: Serena <serenabertozzi@protonmail.com>

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,26 @@
 require "game"
 
 describe Game do
-  subject(:game) { Game.new(:max, :serena) }
   let(:max) { double :player }
   let(:serena) { double :player }
+  subject(:game) { Game.new(max, serena) }
 
   describe ".attack" do
     it "reduces the victims HP by 10" do
       expect(serena).to receive(:receive_attack)
       game.attack(serena)
+    end
+  end
+
+  describe "#player_1" do
+    it "displays player one" do
+      expect(game.player_1).to eq(max)
+    end
+  end
+
+  describe "#player_2" do
+    it "displays player two" do
+      expect(game.player_2).to eq(serena)
     end
   end
 end
